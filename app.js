677a31ab9acb1c1e59fd3c1206d8eff096e4bcf3
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2023-12-12 14:46:59 +0530

    new

---
+++ app.js
@@ -0,0 +1,11 @@
+// ReactDOM.render("Hello! Welcome to React",container);
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
+
+const container = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);
+
+ 
